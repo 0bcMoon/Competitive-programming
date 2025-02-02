e810da6e1fbe0240c8b3cbef5d2a3b0f11ddd42b
Hicham Benoukaiss <hibenouk@e1r8p14.1337.ma>
2025-02-02 19:12:49 +0100

    sync

---
+++ Codeforces/solve/A_Milya_and_Two_Arrays.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using ll = long long;
+using lpair = std::pair<ll, ll>;
+using ipair = std::pair<int, int>;
+#define all(x) (x).begin(), (x).end()
+void solve()
+{
+	
+}
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	int t;
+	std::cin >> t;
+	while(t--)
+	{
+		solve();
+	}
+	return 0;
+}
